508453ec2f0bd12b579438b8a466fd486b62b76e
weidongshan <weidongshan@qq.com>
2016-05-26 10:33:37 +0800

    v17, 17th_class_template

---
+++ 17th_class_template/template.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+
+template<typename T>
+class AAA {
+private:
+	T t;
+public:
+	void test_func(const T &t);
+	void print(void);
+};
+
+template<typename T> void AAA<T>::test_func(const T &t)
+{
+	this->t = t;
+}
+
+template<typename T>
+void AAA<T>::print(void)
+{
+	cout<<t<<endl;
+}
+
+int main(int argc, char **argv)
+{
+	AAA<int> a;
+
+	a.test_func(1);
+	a.print();
+
+	AAA<double> b;
+
+	b.test_func(1.23);
+	b.print();
+
+	return 0;
+}
+
---
+++ 17th_class_template/template2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+
+template<typename T>
+class AAA {
+private:
+	T t;
+public:
+	void test_func(const T &t);
+	void print(void);
+};
+
+template<typename T> void AAA<T>::test_func(const T &t)
+{
+	this->t = t;
+}
+
+template<typename T>
+void AAA<T>::print(void)
+{
+	cout<<t<<endl;
+}
+
+
+template<>
+class AAA<int> {
+public:
+	void test_func_int(const int & t)
+	{
+		cout<<t<<endl;
+	}
+	void print_int(void);
+};
+
+void AAA<int>::print_int(void)
+{
+	cout<<"for test"<<endl;
+}
+
+int main(int argc, char **argv)
+{
+	AAA<int> a;
+
+	a.test_func_int(1);
+	a.print_int();
+
+	AAA<double> b;
+
+	b.test_func(1.23);
+	b.print();
+
+	return 0;
+}
+
